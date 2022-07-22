042705416acccf017797ae906dd4410fcf5d769b
ekjoy <eldhosekjoy@outlook.com>
2022-07-22 13:48:11 +0100

    Initial Commit

---
+++ DNetCoreMVCRazorSignalR/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DNetCoreMVCRazorSignalR.Pages
+{
+	public class IndexModel : PageModel
+	{
+		private readonly ILogger<IndexModel> _logger;
+		private readonly UserManager<IdentityUser> _userManager;
+		public List<SelectListItem>? Users { get; set; }
+		public string? MyUser {get;set;}
+
+		public IndexModel(ILogger<IndexModel> logger, UserManager<IdentityUser> userManager)
+		{
+			_logger = logger;
+			_userManager = userManager;
+		}
+
+		public void OnGet()
+		{
+			Users = _userManager.Users.ToList()
+				.Select(x => new SelectListItem {Text = x.UserName, Value =x.UserName })
+				.OrderBy(s=>s.Text).ToList();
+			MyUser = User.Identity?.Name;
+		}
+	}
+}
---
+++ DNetCoreMVCRazorSignalR/SignalRHubs/ChatHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace DNetCoreMVCRazorSignalR.SignalRHubs
+{
+	public class ChatHub : Hub
+	{
+		public override Task OnConnectedAsync()
+		{
+			return base.OnConnectedAsync();
+		}
+
+		public async Task SendMessageAsync(string sender, string message)
+		{
+			await Clients.All.SendAsync("ReceiveMessage",sender, message);
+		}
+
+		public async Task SendMessageToGroupAsync(string sender,string receiver, string message)
+		{
+			await Clients.Group(receiver).SendAsync("ReceiveMessage",sender, message);
+		}
+	}
+}
